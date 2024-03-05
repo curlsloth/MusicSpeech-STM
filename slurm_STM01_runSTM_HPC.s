9badc0006fe1605648ae9c108c61ea2594612164
Andrew Chang <52460429+curlsloth@users.noreply.github.com>
2024-03-05 17:13:50 -0500

    modify slurm

--- slurm_STM01_runSTM_HPC.s
+++ slurm_STM01_runSTM_HPC.s
@@ -4,7 +4,7 @@
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=2GB
-#SBATCH --time=00:00:30
+#SBATCH --time=00:05:00
 #SBATCH --output=HPC/slurm/output_%A_%a.out
 #SBATCH --mail-user=ac8888@nyu.edu
 #SBATCH --mail-type=FAIL
