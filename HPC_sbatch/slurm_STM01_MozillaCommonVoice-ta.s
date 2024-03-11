de1b2bb3caf20b6a145828dbd47ac839c9de4df8
Andrew Chang <52460429+curlsloth@users.noreply.github.com>
2024-03-11 10:15:06 -0400

    updating .s sbatch files (done)

---
+++ HPC_sbatch/slurm_STM01_MozillaCommonVoice-ta.s
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#SBATCH --job-name=MozillaCommonVoice-ta
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=8GB
+#SBATCH --time=00:40:00
+#SBATCH --output=HPC_slurm/MozillaCommonVoice-ta/slurm_%A_%a.out
+#SBATCH --mail-user=ac8888@nyu.edu
+#SBATCH --mail-type=END
+
+module purge
+module load matlab/2023b
+module load libsndfile/intel/1.0.31
+
+# MATLAB command with input arguments
+matlab -nodisplay -r "STM01_runSTM_HPC('STM_output/corpMetaData/MozillaCommonVoice-ta.mat','MozillaCommonVoice/ta', $SLURM_ARRAY_TASK_ID); exit;"
+# Run this: sbatch --array=0-99 HPC_sbatch/slurm_STM01_MozillaCommonVoice-ta.s
