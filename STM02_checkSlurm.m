7f91bee26d68888d2ebfc03f2e12b2e4dbcecd7e
Andrew Chang <52460429+curlsloth@users.noreply.github.com>
2024-03-12 11:38:16 -0400

    add date range to STM02_checkSlurm

--- STM02_checkSlurm.m
+++ STM02_checkSlurm.m
@@ -1,10 +1,17 @@
 
 
 listing = dir('HPC_slurm/**/*.out');
+
+% Specify the date and time range
+start_date = datetime('2024-03-12 00:00:00');
+end_date = datetime('2024-03-12 23:59:59');
+
 for nOut = 1:length(listing)
-    output_filename = [listing(nOut).folder,'/',listing(nOut).name];
-    check_code = sum(contains(readlines(output_filename),'Successful STM!'));
-    if ~check_code
-        disp(['error here: ', output_filename])
+    if (datetime(listing(nOut).date) > start_date) && (datetime(listing(nOut).date) < end_date)
+        output_filename = [listing(nOut).folder,'/',listing(nOut).name];
+        check_code = sum(contains(readlines(output_filename),'Successful STM!'));
+        if ~check_code
+            disp(['error here: ', output_filename])
+        end
     end
 end
