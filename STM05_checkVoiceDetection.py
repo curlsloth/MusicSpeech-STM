27db1f45727a264246f2efca06a740b5652e23e5
Andrew Chang <52460429+curlsloth@users.noreply.github.com>
2024-04-15 21:37:48 -0400

    add vocal_music_demucs to meta data

---
+++ STM05_checkVoiceDetection.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 15 09:39:07 2024
+
+@author: andrewchang
+"""
+
+# %% import modules and set corpus name
+
+import os
+import datetime
+import pandas as pd
+
+# %% functions
+# check slurm output
+
+def check_slurm(corpus_name, start_time, end_time):
+    # Directory containing the .out files
+    directory = 'HPC_slurm/STM04/'+corpus_name
+    
+    # Function to check if the string is present in a file
+    def check_string_in_file(file_path, target_string):
+        with open(file_path, 'r') as file:
+            content = file.read()
+            if target_string in content:
+                return True
+            else:
+                return False
+    
+    # Target string to search for
+    target_string = "Demucs voice recognition done!"
+    
+    # List all files in the directory
+    file_list = os.listdir(directory)
+    
+    # Filter out only the .out files
+    out_files = [file for file in file_list if file.endswith('.out')]
+    
+    # Filter files within the specified time range
+    filtered_files = []
+    for file in out_files:
+        file_path = os.path.join(directory, file)
+        file_creation_time = datetime.datetime.fromtimestamp(os.path.getmtime(file_path))
+        if start_time <= file_creation_time <= end_time:
+            filtered_files.append(file)
+    
+    # Iterate over each filtered .out file and check for the target string
+    files_without_string = []
+    for file in filtered_files:
+        file_path = os.path.join(directory, file)
+        if not check_string_in_file(file_path, target_string):
+            files_without_string.append(file)
+    
+    # Print files that don't contain the target string
+    if files_without_string:
+        print("Files generated between "+str(start_time)+" and "+str(end_time)+" without the string 'Demucs voice recognition done!':")
+        for file in files_without_string:
+            print(file)
+    elif len(filtered_files)==0:
+        print("No files generated between "+str(start_time)+" and "+str(end_time))
+    else:
+        print("All files generated between "+str(start_time)+" and "+str(end_time)+" contain the string 'Demucs voice recognition done!'")
+    
+    
+    
+# update the original meta file
+def update_meta_file(corpus_name):
+    corpus = 'metaData_'+corpus_name
+    demucs_output_directory = 'metaTables/vocal_music_demucs/'
+    demucs_output_file_list = os.listdir(os.path.join(demucs_output_directory,corpus))
+    
+    file_list = []
+    for file in demucs_output_file_list:
+        file_path = os.path.join(demucs_output_directory, corpus, file)
+        file_list.append(pd.read_csv(file_path, names = ['filepath', 'demucs_voice']))
+    
+    df_demucs_total = pd.concat(file_list, ignore_index=True)
+    df_meta = pd.read_csv('metaTables/'+corpus+'.csv',index_col=0)
+    if 'demucs_voice' not in df_meta.columns:
+        df_metaData_demucs = pd.read_csv('metaTables/'+corpus+'.csv',index_col=0).merge(df_demucs_total)
+        df_metaData_demucs.to_csv('metaTables/'+corpus+'.csv')
+        print('metaTables/'+corpus+'.csv file updated with demucs voice data')
+
+
+# %% run the functions
+
+# corpus_name = 'fma_large'
+# corpus_name = 'ismir04_genre'
+# corpus_name = 'MagnaTagATune'
+corpus_name = 'MTG-Jamendo'
+
+start_time = datetime.datetime(2024, 4, 15, 15, 0)
+end_time = datetime.datetime(2024, 4, 15, 23, 0)
+check_slurm(corpus_name, start_time, end_time)
+update_meta_file(corpus_name)
