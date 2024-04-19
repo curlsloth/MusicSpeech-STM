cea61fba5abe03cf63d8d10929037f44156c97ff
Andrew Chang <52460429+curlsloth@users.noreply.github.com>
2024-04-19 14:31:54 -0400

    make STM06 much faster

--- STM06_STMpreproc.py
+++ STM06_STMpreproc.py
@@ -8,38 +8,60 @@ Created on Thu Apr 18 21:49:01 2024
 
 import scipy.io
 import numpy as np
-import matplotlib.pyplot as plt
-mat_data = scipy.io.loadmat('STM_output/MATs/music_mat_wl4_Albouy2020Science/English_S01_M01_MS2024.mat')
-survey_data = scipy.io.loadmat('STM_output/Survey/music_params_CD/01 Andante-Adagio-Allegretto_Params.mat')
+import time
+import pandas as pd
 
-data = mat_data['indMS']
-
-x_axis = survey_data['Params'][0]['x_axis'][0][0]
-y_axis = survey_data['Params'][0]['y_axis'][0][0]
-
-xmin = np.argmin(np.abs(x_axis - -30))
-xmax = np.argmin(np.abs(x_axis - 30))
-ymin = np.argmin(np.abs(y_axis - 0))
-ymax = np.argmin(np.abs(y_axis - 9))
-
-
-# x_neg30_ind = 130 # -30 Hz
-# x_pos30_ind = 371 # 30 Hz
-# y_0_ind = 75 # 0 cyc/oct
-# y_pos9_ind = 124 # 0 cyc/oct
-
-x_axis_small = x_axis[xmin:xmax+1]
-y_axis_small = y_axis[ymin:ymax+1]
 
 def preproSTM(data, xmin, xmax, ymin, ymax):
-    data_small = data[ymin:ymax+1, xmin:xmax+1]
-    data_small_db = 10 * np.log10(data_small) # power dB
-    data_small_db_normalized = (data_small_db - data_small_db.min()) / (data_small_db.max() - data_small_db.min())
-    return data_small_db_normalized
+    # select the middle range, dB transformation, normalize to [0,1], make it as float 32
+    data_small_db = 10 * np.log10(data[ymin:ymax+1, xmin:xmax+1]) # power dB
+    return np.float32((data_small_db - data_small_db.min()) / (data_small_db.max() - data_small_db.min()))
 
 
-plt.pcolormesh(x_axis_small, y_axis_small, preproSTM(data, xmin, xmax, ymin, ymax))  # You can choose any colormap you prefer
-plt.colorbar()  # Add a colorbar for reference
-plt.show()
+def stack_STM(metaData):
+    stm_stacked_list = []
+    df = pd.read_csv(metaData,index_col=0)    
+    
+    # hard-coded parameters. Use the code below to modify it
+    # xrange = [-20, 20]
+    # yrange = [0,7]
+    xmin=170
+    xmax=330
+    ymin=75
+    ymax=112
+
+    for mat_file in df['mat_filename']:
+        data = scipy.io.loadmat(mat_file)['indMS']
+        if data.shape == (150, 500):
+            stm_stacked_list.append(preproSTM(data, xmin, xmax, ymin, ymax).flatten())
+        else:
+            print('indMS size wrong: '+str(data.shape)+mat_file)
+    return np.vstack(stm_stacked_list)
 
 
+t_start = time.time()
+stm_stacked = stack_STM('metaTables/metaData_BibleTTS-hausa.csv')
+print('time elapsed: '+str(time.time()-t_start)+' seconds')
+
+# %% plot 2D STM
+# import matplotlib.pyplot as plt
+# plt.pcolormesh(x_axis_small, y_axis_small, data_proc)  # You can choose any colormap you prefer
+# plt.colorbar()  # Add a colorbar for reference
+# plt.show()
+
+# %% load one survey data to set the axis parameters
+# df = pd.read_csv('metaTables/metaData_BibleTTS-hausa.csv',index_col=0)
+# survey_file = df['mat_filename'][0].replace('MATs','Survey').replace('mat_wl4','params').replace('MS2024','Params')
+# survey_data = scipy.io.loadmat(survey_file)
+# x_axis = survey_data['Params'][0]['x_axis'][0][0]
+# y_axis = survey_data['Params'][0]['y_axis'][0][0]
+
+# xrange = [-20, 20]
+# yrange = [0,7]
+
+# xmin = np.argmin(np.abs(x_axis - xrange[0]))
+# xmax = np.argmin(np.abs(x_axis - xrange[1]))
+# ymin = np.argmin(np.abs(y_axis - yrange[0]))
+# ymax = np.argmin(np.abs(y_axis - yrange[1]))
+# x_axis_small = x_axis[xmin:xmax+1]
+# y_axis_small = y_axis[ymin:ymax+1]
