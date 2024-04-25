828c3090f2d7a8b2524191aba61de72b14b6b35a
Andrew Chang <52460429+curlsloth@users.noreply.github.com>
2024-04-25 12:50:53 -0400

    working on STM08

--- STM08_plot_tSNE_PCA.py
+++ STM08_plot_tSNE_PCA.py
@@ -8,20 +8,13 @@ Created on Mon Apr 22 21:19:01 2024
 
 import numpy as np
 import pandas as pd
-from sklearn.manifold import TSNE
-from sklearn.decomposition import IncrementalPCA
-from sklearn.preprocessing import StandardScaler
-from sklearn.pipeline import make_pipeline
 from joblib import dump, load
 import datetime
-import sys
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-plt.rcParams['figure.dpi'] = 300
-plt.rcParams['savefig.dpi'] = 300
 
-# %% corpus data cleaning
+# %% corpus lists
 
 corpus_speech_list = ['BibleTTS/akuapem-twi',
     'BibleTTS/asante-twi',
@@ -142,12 +135,47 @@ corpus_speech_list.sort()
 corpus_music_list.sort()
 corpus_env_list.sort()
 
+
+# %% shared functions
+def unify_gender(df):
+    df['gender'] = df['gender'].replace({
+        'm':'male',
+        'M':'male',
+        'f':'female',
+        'F':'female',
+        'other':np.nan,
+        'O':np.nan,
+        })
+    return df
+
+# %% speech data cleaning
+
 speech_corp_list = []
 for corp in corpus_speech_list:
     metafile = 'metaTables/metaData_'+corp.replace('/', '-')+'.csv'
     speech_corp_list.append(pd.read_csv(metafile,index_col=0))
 speech_corp_df = pd.concat(speech_corp_list, ignore_index=True)
-    
+
+speech_corp_df = unify_gender(speech_corp_df)
+
+gender_count = speech_corp_df['gender'].value_counts()
+
+
+
+print("Total length of speech recordings: "+ str(round(speech_corp_df['totalLengCur'].sum()/(60*60),2))+" hours")
+print("Number of speech recordings: "+ str(len(speech_corp_df)))
+print("Number of unique speakers: "+ str(len(speech_corp_df['speaker/artist'].unique())))
+
+print("Male ratio: "+str(round(100*gender_count['male']/len(speech_corp_df), 2))+"%")
+print("Female ratio: "+str(round(100*gender_count['female']/len(speech_corp_df), 2))+"%")
+print("NaN ratio: " + str(round(100*sum(speech_corp_df['gender'].isna())/len(speech_corp_df),2))+"%")
+
+languages = speech_corp_df['LangOrInstru'].unique()
+languages.sort()    
+print("Number of languages: "+str(len(languages)))
+print(languages)
+
+# %% music data cleaning
 music_corp_list = []
 for corp in corpus_music_list:
     metafile = 'metaTables/metaData_'+corp.replace('/', '-')+'.csv'
@@ -164,28 +192,6 @@ for corp in corpus_music_list:
     music_corp_list.append(df)
 music_corp_df = pd.concat(music_corp_list, ignore_index=True)
 
-df_SONYC = pd.read_csv('metaTables/metaData_SONYC.csv',index_col=0)
-
-speech_corp_df['gender'].replace({
-    'm':'male',
-    'M':'male',
-    'f':'female',
-    'F':'female',
-    'other':np.nan,
-    'O':np.nan},
-    inplace=True)
-
-
-speech_corp_df['totalLengCur'].sum()
-music_corp_df['totalLengCur'].sum()
-
-sum(speech_corp_df['gender']=='male')/len(speech_corp_df)
-sum(speech_corp_df['gender']=='female')/len(speech_corp_df)
-sum(speech_corp_df['gender'].isna())/len(speech_corp_df)
-
-languages = speech_corp_df['LangOrInstru'].unique()
-languages.sort()
-
 genre_columns = pd.concat([music_corp_df['genre_1'], music_corp_df['genre_2'], music_corp_df['genre_3']], axis=0)
 genre_columns.replace({
     np.nan:'unlabeled',
@@ -200,8 +206,47 @@ genre_columns = genre_columns.apply(lambda x: x.title())
 genres = genre_columns.unique()
 genres.sort()
 
+music_corp_df = unify_gender(music_corp_df)
+
+print("Total length of music recordings: "+ str(round(music_corp_df['totalLengCur'].sum()/(60*60),2))+" hours")
+print("Number of music recordings: "+ str(len(music_corp_df)))
+print("Number of unique musicians/ensembles: "+ str(len(music_corp_df['speaker/artist'].unique())))
+
+
+print("Number of genres: "+str(len(genres)))
+print(genres)
+
+# %% env sounds
+
+df_SONYC = pd.read_csv('metaTables/metaData_SONYC.csv',index_col=0)
+print("Total length of environmental recordings: "+ str(round(df_SONYC['totalLengCur'].sum()/(60*60),2))+" hours")
+print("Number of environmental recordings: "+ str(len(df_SONYC)))
+
+
 
 all_corp_df = pd.concat([speech_corp_df, music_corp_df, df_SONYC])
+
+# %% tone languages
+# reference : https://wals.info/feature/13A?s=20&z3=3000&z2=2999&z1=2998&tg_format=map&v1=cfff&v2=cf6f&v3=cd00#2/19.3/152.8
+complex_tone_lang_list = [
+    'Cantonese',
+    'Mandarin-China', 
+    'Mandarin-Taiwan',
+    'Taiwanese',
+    'Thai',
+    'Vietnamese',
+    'Yoruba'
+    ]
+simple_tone_lang_list = [
+    'Ewe',
+    'Hadza',
+    'Hausa',
+    'Igbo',
+    'Japanese',
+    'Latvian',
+    ]
+
+tone_lang_list = complex_tone_lang_list+simple_tone_lang_list
 
 # %% plot PCA
 
@@ -214,7 +259,7 @@ plt.show()
 
 # %% load t-SNE and preprocessing
 
-tsne_folder = 'model/tsne/perplexity200_2024-04-24_07-23/'
+tsne_folder = 'model/tsne/perplexity400_2024-04-24_18-02/'
 
 tsne = load(tsne_folder+'tsne_model.joblib')
 df_tsne = pd.DataFrame()
@@ -227,6 +272,14 @@ song_mask = pd.concat([song_mask, pd.Series([False]* (len(tsne.embedding_) - len
 song_mask = song_mask.reset_index(drop=True)
 df_tsne.loc[song_mask, 'category'] = 'song'
 
+toneLang_mask = (all_corp_df['corpus_type'] == 'speech') & (all_corp_df['LangOrInstru'].isin(tone_lang_list))
+nontoneLang_mask = (all_corp_df['corpus_type'] == 'speech') & (~all_corp_df['LangOrInstru'].isin(tone_lang_list))
+toneLang_mask = pd.concat([toneLang_mask, pd.Series([False]* (len(tsne.embedding_) - len(all_corp_df)))])
+nontoneLang_mask = pd.concat([nontoneLang_mask, pd.Series([False]* (len(tsne.embedding_) - len(all_corp_df)))])
+toneLang_mask = toneLang_mask.reset_index(drop=True)
+nontoneLang_mask = nontoneLang_mask.reset_index(drop=True)
+df_tsne.loc[toneLang_mask, 'category'] = 'speech: tonal'
+df_tsne.loc[nontoneLang_mask, 'category'] = 'speech: non-tonal'
 
 # %% plot kernal 
 df_tsne_filtered = df_tsne[(df_tsne['tSNE-1'].between(-60,60)) & (df_tsne['tSNE-2'].between(-60,60))]
