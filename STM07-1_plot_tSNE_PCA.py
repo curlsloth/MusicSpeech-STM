4ac0d6e2461b81c0ae37a1315fd881d24fade550
Andrew Chang <52460429+curlsloth@users.noreply.github.com>
2024-04-28 11:38:34 -0400

    fir kdm plot issue

--- STM07-1_plot_tSNE_PCA.py
+++ STM07-1_plot_tSNE_PCA.py
@@ -22,16 +22,16 @@ all_corp_df = pd.concat([speech_corp_df1, speech_corp_df2, music_corp_df, df_SON
 
 # %% plot PCA
 
-pipeline = load('model/allSTM_pca-pipeline_2024-04-22_08-43.joblib')
-pca = pipeline['incrementalpca']
+# pipeline = load('model/allSTM_pca-pipeline_2024-04-22_08-43.joblib')
+# pca = pipeline['incrementalpca']
 
 
-plt.plot(list(range(1,len(pca.explained_variance_ratio_[:5000])+1)), np.cumsum(pca.explained_variance_ratio_[:5000]))
-plt.show()
+# plt.plot(list(range(1,len(pca.explained_variance_ratio_[:5000])+1)), np.cumsum(pca.explained_variance_ratio_[:5000]))
+# plt.show()
 
 # %% load t-SNE and preprocessing
 
-tsne_folder = 'model/tsne/perplexity400_2024-04-24_18-02/'
+tsne_folder = 'model/tsne/perplexity100_2024-04-27_10-26/'
 
 
 tsne = load(tsne_folder+'tsne_model.joblib')
@@ -41,8 +41,28 @@ df_tsne['tSNE-2'] = tsne.embedding_[:,1]
 df_tsne['category'] = pd.concat([all_corp_df['corpus_type'], 
                                  pd.Series(['env_aug'] * (len(tsne.embedding_) - len(all_corp_df)))], ignore_index=True)
 
-# %% plot kernal 
-df_tsne_filtered = df_tsne[(df_tsne['tSNE-1'].between(-60,60)) & (df_tsne['tSNE-2'].between(-60,60))]
+# %% scatter plot
+with plt.style.context('seaborn-v0_8-notebook'):
+    plt.rcParams['figure.dpi'] = 300
+    plt.rcParams['savefig.dpi'] = 300
+
+    fig = plt.figure(figsize=(10,10))
+    ax = fig.add_subplot(1, 1, 1)
+    
+    kde = sns.scatterplot(
+        x="tSNE-1", 
+        y="tSNE-2",
+        data=df_tsne,
+        ax=ax,
+        hue="category", 
+        alpha=0.1
+    )
+    ax.set_xlim(-75, 75)
+    ax.set_ylim(-75, 75)
+    plt.show()
+
+# %% plot kernal density
+df_tsne_filtered = df_tsne[(df_tsne['tSNE-1'].between(-70, 70)) & (df_tsne['tSNE-2'].between(-70,70))]
 
 with plt.style.context('seaborn-v0_8-notebook'):
     plt.rcParams['figure.dpi'] = 300
@@ -60,10 +80,12 @@ with plt.style.context('seaborn-v0_8-notebook'):
         fill=False,
         levels=10,
         alpha=0.5,
-        threshold=0
+        threshold=0,
+        common_norm=False,
         # bw_adjust=0.1,
     )
-    ax.set_xlim(-50, 60)
-    ax.set_ylim(-55, 50)
+    # ax.set_xlim(-75, 75)
+    # ax.set_ylim(-75, 75)
     plt.show()
     fig.savefig(tsne_folder+'kdeplot_'+datetime.datetime.now().strftime("%Y-%m-%d_%H-%M")+'.png')
+    
