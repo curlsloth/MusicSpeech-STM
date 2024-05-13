9965919138a32ce711536c22c749805f93c39eaa
Andrew Chang <52460429+curlsloth@users.noreply.github.com>
2024-05-13 11:10:52 -0400

    replace librosa with soundfile

--- STM10_YAMNet_emb.py
+++ STM10_YAMNet_emb.py
@@ -8,8 +8,9 @@ import scipy
 import numpy as np
 import pandas as pd
 import time
-import librosa
+# import librosa
 import sys
+import soundfile as sf
 
 # import matplotlib.pyplot as plt
 # from IPython.display import Audio
@@ -167,7 +168,7 @@ def run_YAMNet(corp):
     for n_row in range(len(df_meta)):
         try:
             filename = df_meta['filepath'].iloc[n_row]
-            frame_offset = df_meta['startPoint'].iloc[n_row]-1
+            frame_start = df_meta['startPoint'].iloc[n_row]-1
             frame_end = df_meta['endPoint'].iloc[n_row]-1
             if corp=='EUROM':
                 with open(filename, 'rb') as fid:
@@ -175,8 +176,9 @@ def run_YAMNet(corp):
                 waveform = waveform/max(abs(waveform))
                 sr = 20000
             else:
-                waveform , sr = librosa.load(filename, sr=None, mono=True)
-            waveform = waveform [frame_offset:frame_end]
+                waveform , sr = sf.read(filename, start=frame_start, stop=frame_end+1)
+                waveform = waveform.mean(axis=1)
+
             _, waveform = ensure_sample_rate(sr, waveform) # convert to sr=16000
             scores, embeddings, _ = model(waveform) # use YAMNET to score the audio waveform
             scores_stacked_list.append(scores.numpy().mean(axis=0))
