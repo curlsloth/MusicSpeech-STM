2b21ab4a8203d6781ffad11d023e971ed5bbc3b5
Andrew Chang <52460429+curlsloth@users.noreply.github.com>
2024-09-06 14:30:31 -0400

    update ablation: remove the filtered STM regions

--- STM08gpu_MLP_STM_corpus.py
+++ STM08gpu_MLP_STM_corpus.py
@@ -313,8 +313,8 @@ def auto_git_push(path):
 
 
 # %% build model
-n_feat = 2420
-n_target = 6
+# n_feat = 2420
+# n_target = 6
 
 class hyperModel_drop(kt.HyperModel):
 
@@ -432,172 +432,175 @@ class hyperModel_LN(kt.HyperModel):
             **kwargs,
         )
 
-# %% set the tuner
+# %% Run the script
 
-if sys.argv[1]=='0':
-    train_dataset, val_dataset, test_dataset, n_feat, n_target = prepData()
-    hm = hyperModel_drop()
-    directory = "model/STM/MLP_corpora_categories/Dropout/ROC-AUC"
-    objective="val_auc"
-    early_stop = "val_auc"
-elif sys.argv[1]=='1':
-    train_dataset, val_dataset, test_dataset, n_feat, n_target = prepData()
-    hm = hyperModel_LN()
-    directory = "model/STM/MLP_corpora_categories/LayerNormalization/ROC-AUC"
-    objective="val_auc"
-    early_stop = "val_auc"
-elif sys.argv[1]=='2':
-    train_dataset, val_dataset, test_dataset, n_feat, n_target = prepData()
-    hm = hyperModel_drop()
-    directory = "model/STM/MLP_corpora_categories/Dropout/macroF1"
-    objective = kt.Objective("val_macro_f1_score", direction="max")
-    early_stop = "val_f1_score"
-elif sys.argv[1]=='3':
-    train_dataset, val_dataset, test_dataset, n_feat, n_target = prepData()
-    hm = hyperModel_LN()
-    directory = "model/STM/MLP_corpora_categories/LayerNormalization/macroF1"
-    objective = kt.Objective("val_macro_f1_score", direction="max")
-    early_stop = "val_f1_score"
-elif sys.argv[1]=='4': # downsample nontonal speech
-    train_dataset, val_dataset, test_dataset, n_feat, n_target = prepData(ds_nontonal_speech=True)
-    hm = hyperModel_drop()
-    directory = "model/STM/MLP_corpora_categories/Dropout/ROC-AUC/downsample"
-    objective="val_auc"
-    early_stop = "val_auc"
-elif sys.argv[1]=='5': # downsample nontonal speech
-    train_dataset, val_dataset, test_dataset, n_feat, n_target = prepData(ds_nontonal_speech=True)
-    hm = hyperModel_LN()
-    directory = "model/STM/MLP_corpora_categories/LayerNormalization/ROC-AUC/downsample"
-    objective="val_auc"
-    early_stop = "val_auc"
-elif sys.argv[1]=='6': # downsample nontonal speech
-    train_dataset, val_dataset, test_dataset, n_feat, n_target = prepData(ds_nontonal_speech=True)
-    hm = hyperModel_drop()
-    directory = "model/STM/MLP_corpora_categories/Dropout/macroF1/downsample"
-    objective = kt.Objective("val_macro_f1_score", direction="max")
-    early_stop = "val_f1_score"
-elif sys.argv[1]=='7': # downsample nontonal speech
-    train_dataset, val_dataset, test_dataset, n_feat, n_target = prepData(ds_nontonal_speech=True)
-    hm = hyperModel_LN()
-    directory = "model/STM/MLP_corpora_categories/LayerNormalization/macroF1/downsample"
-    objective = kt.Objective("val_macro_f1_score", direction="max")
-    early_stop = "val_f1_score"
-elif 8<=int(sys.argv[1])<=56:
-    c_index = int(sys.argv[1])-8
-    n_xcut = 7
-    n_ycut = 7
-    cond_shape = (n_xcut, n_ycut)
-    total_models = np.prod(cond_shape)
+if __name__ == "__main__":
+
+    # % set the tuner
+    if sys.argv[1]=='0':
+        train_dataset, val_dataset, test_dataset, n_feat, n_target = prepData()
+        hm = hyperModel_drop()
+        directory = "model/STM/MLP_corpora_categories/Dropout/ROC-AUC"
+        objective="val_auc"
+        early_stop = "val_auc"
+    elif sys.argv[1]=='1':
+        train_dataset, val_dataset, test_dataset, n_feat, n_target = prepData()
+        hm = hyperModel_LN()
+        directory = "model/STM/MLP_corpora_categories/LayerNormalization/ROC-AUC"
+        objective="val_auc"
+        early_stop = "val_auc"
+    elif sys.argv[1]=='2':
+        train_dataset, val_dataset, test_dataset, n_feat, n_target = prepData()
+        hm = hyperModel_drop()
+        directory = "model/STM/MLP_corpora_categories/Dropout/macroF1"
+        objective = kt.Objective("val_macro_f1_score", direction="max")
+        early_stop = "val_f1_score"
+    elif sys.argv[1]=='3':
+        train_dataset, val_dataset, test_dataset, n_feat, n_target = prepData()
+        hm = hyperModel_LN()
+        directory = "model/STM/MLP_corpora_categories/LayerNormalization/macroF1"
+        objective = kt.Objective("val_macro_f1_score", direction="max")
+        early_stop = "val_f1_score"
+    elif sys.argv[1]=='4': # downsample nontonal speech
+        train_dataset, val_dataset, test_dataset, n_feat, n_target = prepData(ds_nontonal_speech=True)
+        hm = hyperModel_drop()
+        directory = "model/STM/MLP_corpora_categories/Dropout/ROC-AUC/downsample"
+        objective="val_auc"
+        early_stop = "val_auc"
+    elif sys.argv[1]=='5': # downsample nontonal speech
+        train_dataset, val_dataset, test_dataset, n_feat, n_target = prepData(ds_nontonal_speech=True)
+        hm = hyperModel_LN()
+        directory = "model/STM/MLP_corpora_categories/LayerNormalization/ROC-AUC/downsample"
+        objective="val_auc"
+        early_stop = "val_auc"
+    elif sys.argv[1]=='6': # downsample nontonal speech
+        train_dataset, val_dataset, test_dataset, n_feat, n_target = prepData(ds_nontonal_speech=True)
+        hm = hyperModel_drop()
+        directory = "model/STM/MLP_corpora_categories/Dropout/macroF1/downsample"
+        objective = kt.Objective("val_macro_f1_score", direction="max")
+        early_stop = "val_f1_score"
+    elif sys.argv[1]=='7': # downsample nontonal speech
+        train_dataset, val_dataset, test_dataset, n_feat, n_target = prepData(ds_nontonal_speech=True)
+        hm = hyperModel_LN()
+        directory = "model/STM/MLP_corpora_categories/LayerNormalization/macroF1/downsample"
+        objective = kt.Objective("val_macro_f1_score", direction="max")
+        early_stop = "val_f1_score"
+    elif 8<=int(sys.argv[1])<=56:
+        c_index = int(sys.argv[1])-8
+        n_xcut = 7
+        n_ycut = 7
+        cond_shape = (n_xcut, n_ycut)
+        total_models = np.prod(cond_shape)
+            
+        # Calculate the indices for each dimension
+        i1 = (c_index % cond_shape[1]) 
+        c_index //= cond_shape[1]
+        i0 = c_index 
         
-    # Calculate the indices for each dimension
-    i1 = (c_index % cond_shape[1]) 
-    c_index //= cond_shape[1]
-    i0 = c_index 
-    
-    ablation_params = {
-        'x_lowcutoff': None,
-        'x_highcutoff': i0,
-        'y_lowcutoff': None,
-        'y_highcutoff': i1,
-        }
-    
-    train_dataset, val_dataset, test_dataset, n_feat, n_target = prepData(ablation_params = ablation_params)
-    hm = hyperModel_LN()
-    directory = "model/STM/MLP_corpora_categories/LayerNormalization/macroF1/ablation/xhighcutoff"+str(i0)+"_yhighcutoff"+str(i1)
-    objective = kt.Objective("val_macro_f1_score", direction="max")
-    early_stop = "val_f1_score"
-elif 57<=int(sys.argv[1])<=105:
-    c_index = int(sys.argv[1])-57
-    n_xcut = 7
-    n_ycut = 7
-    cond_shape = (n_xcut, n_ycut)
-    total_models = np.prod(cond_shape)
+        ablation_params = {
+            'x_lowcutoff': None,
+            'x_highcutoff': i0,
+            'y_lowcutoff': None,
+            'y_highcutoff': i1,
+            }
         
-    # Calculate the indices for each dimension
-    i1 = (c_index % cond_shape[1])
-    c_index //= cond_shape[1]
-    i0 = c_index
+        train_dataset, val_dataset, test_dataset, n_feat, n_target = prepData(ablation_params = ablation_params)
+        hm = hyperModel_LN()
+        directory = "model/STM/MLP_corpora_categories/LayerNormalization/macroF1/ablation/xhighcutoff"+str(i0)+"_yhighcutoff"+str(i1)
+        objective = kt.Objective("val_macro_f1_score", direction="max")
+        early_stop = "val_f1_score"
+    elif 57<=int(sys.argv[1])<=105:
+        c_index = int(sys.argv[1])-57
+        n_xcut = 7
+        n_ycut = 7
+        cond_shape = (n_xcut, n_ycut)
+        total_models = np.prod(cond_shape)
+            
+        # Calculate the indices for each dimension
+        i1 = (c_index % cond_shape[1])
+        c_index //= cond_shape[1]
+        i0 = c_index
+        
+        ablation_params = {
+            'x_lowcutoff': i0,
+            'x_highcutoff': None,
+            'y_lowcutoff': i1,
+            'y_highcutoff': None,
+            }
+        
+        train_dataset, val_dataset, test_dataset, n_feat, n_target = prepData(ablation_params = ablation_params)
+        hm = hyperModel_LN()
+        directory = "model/STM/MLP_corpora_categories/LayerNormalization/macroF1/ablation/xlowcutoff"+str(i0)+"_ylowcutoff"+str(i1)
+        objective = kt.Objective("val_macro_f1_score", direction="max")
+        early_stop = "val_f1_score"
+    else:
+        print("Index out of range of models. Nothing executed.")
+        sys.exit(0)
+        
+    time_stamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M")
     
-    ablation_params = {
-        'x_lowcutoff': i0,
-        'x_highcutoff': None,
-        'y_lowcutoff': i1,
-        'y_highcutoff': None,
-        }
     
-    train_dataset, val_dataset, test_dataset, n_feat, n_target = prepData(ablation_params = ablation_params)
-    hm = hyperModel_LN()
-    directory = "model/STM/MLP_corpora_categories/LayerNormalization/macroF1/ablation/xlowcutoff"+str(i0)+"_ylowcutoff"+str(i1)
-    objective = kt.Objective("val_macro_f1_score", direction="max")
-    early_stop = "val_f1_score"
-else:
-    print("Index out of range of models. Nothing executed.")
-    sys.exit(0)
+    # Prepare a directory to store all the checkpoints.
+    checkpoint_dir = directory+"/ckpt/"+time_stamp
+    if not os.path.exists(checkpoint_dir):
+        os.makedirs(checkpoint_dir)
     
-time_stamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M")
-
-
-# Prepare a directory to store all the checkpoints.
-checkpoint_dir = directory+"/ckpt/"+time_stamp
-if not os.path.exists(checkpoint_dir):
-    os.makedirs(checkpoint_dir)
-
-## Disable GPU (not enough usage)
-# Create a MirroredStrategy.
-# strategy = tf.distribute.MirroredStrategy()
-# print('Number of devices: {}'.format(strategy.num_replicas_in_sync))
-
-# with strategy.scope():
-
-
+    ## Disable GPU (not enough usage)
+    # Create a MirroredStrategy.
+    # strategy = tf.distribute.MirroredStrategy()
+    # print('Number of devices: {}'.format(strategy.num_replicas_in_sync))
     
-tuner = kt.BayesianOptimization(
-    hypermodel=hm,
-    objective=objective,
-    num_initial_points=10,
-    max_trials=40,
-    executions_per_trial=3,
-    seed=23,
-    max_retries_per_trial=0,
-    max_consecutive_failed_trials=3,
-    overwrite=True,
-    directory=directory,
-    project_name="MLP_"+time_stamp,
-    )
-tuner.search_space_summary()
-
-tuner.search(
-    train_dataset, 
-    epochs=2, 
-    validation_data=val_dataset,
-    # callbacks=[
-    #     keras.callbacks.EarlyStopping(
-    #         monitor=objective, 
-    #         mode="max",
-    #         patience=5,
-    #         verbose=1,
-    #         ),
-    #     keras.callbacks.ModelCheckpoint(
-    #         filepath=checkpoint_dir + "/ckpt-{epoch}.keras",
-    #         save_freq="epoch",
-    #         ),
-    #     ]
-    )
-
-
-# %% retrain the best model
-tf.keras.backend.clear_session()
-gc.collect()
-
-retrain_dataset = train_dataset.concatenate(val_dataset)
-
-n_best_model = 3
-best_hps = tuner.get_best_hyperparameters(n_best_model)
-for n in range(n_best_model):
-    best_model = hm.build(best_hps[n])
-    best_model.fit(retrain_dataset)
+    # with strategy.scope():
     
-    saving_path = directory+"/"+"MLP_"+time_stamp+"/best_model"+str(n)+".keras"
-    best_model.save(saving_path)
     
-# auto_git_push(directory)
+        
+    tuner = kt.BayesianOptimization(
+        hypermodel=hm,
+        objective=objective,
+        num_initial_points=10,
+        max_trials=40,
+        executions_per_trial=3,
+        seed=23,
+        max_retries_per_trial=0,
+        max_consecutive_failed_trials=3,
+        overwrite=True,
+        directory=directory,
+        project_name="MLP_"+time_stamp,
+        )
+    tuner.search_space_summary()
+    
+    tuner.search(
+        train_dataset, 
+        epochs=2, 
+        validation_data=val_dataset,
+        # callbacks=[
+        #     keras.callbacks.EarlyStopping(
+        #         monitor=objective, 
+        #         mode="max",
+        #         patience=5,
+        #         verbose=1,
+        #         ),
+        #     keras.callbacks.ModelCheckpoint(
+        #         filepath=checkpoint_dir + "/ckpt-{epoch}.keras",
+        #         save_freq="epoch",
+        #         ),
+        #     ]
+        )
+    
+    
+    # retrain the best model
+    tf.keras.backend.clear_session()
+    gc.collect()
+    
+    retrain_dataset = train_dataset.concatenate(val_dataset)
+    
+    n_best_model = 3
+    best_hps = tuner.get_best_hyperparameters(n_best_model)
+    for n in range(n_best_model):
+        best_model = hm.build(best_hps[n])
+        best_model.fit(retrain_dataset)
+        
+        saving_path = directory+"/"+"MLP_"+time_stamp+"/best_model"+str(n)+".keras"
+        best_model.save(saving_path)
+        
+    # auto_git_push(directory)
