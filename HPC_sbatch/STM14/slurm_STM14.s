1c411d93d956c4c9317a6724711fc438c37136a7
Andrew Chang <52460429+curlsloth@users.noreply.github.com>
2024-11-23 20:12:31 -0500

    lower slurm requirements of STM14

--- HPC_sbatch/STM14/slurm_STM14.s
+++ HPC_sbatch/STM14/slurm_STM14.s
@@ -2,9 +2,9 @@
 
 #SBATCH --job-name=STM14
 #SBATCH --nodes=1
-#SBATCH --cpus-per-task=10
+#SBATCH --cpus-per-task=5
 #SBATCH --mem=15GB
-#SBATCH --time=24:00:00
+#SBATCH --time=6:00:00
 #SBATCH --output=HPC_slurm/STM14/STM14_%A_%a.out
 #SBATCH --mail-user=ac8888@nyu.edu
 #SBATCH --mail-type=END
